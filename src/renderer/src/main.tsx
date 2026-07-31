c0bd8b297c7fdf00e0f727d038547b00a9232892
Martin Jeppesen <67677982+swamp2k@users.noreply.github.com>
2026-08-01 00:47:31 +0200

    Load Lite 4 quality styles

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './lite2.css'
 import './lite3.css'
+import './lite4.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
