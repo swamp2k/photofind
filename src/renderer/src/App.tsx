b5a80e3a85cb81d6808fccb06a6b9c26168b609e
Martin Jeppesen <67677982+swamp2k@users.noreply.github.com>
2026-08-18 22:22:43 +0200

    Mount global process indicator

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,5 @@
 import { PhotoFindContextMenuProvider } from './lite/ContextMenu'
+import { GlobalProcessStatus } from './lite/GlobalProcessStatus'
 import { LiteApp } from './lite/LiteApp'
 import { ReviewSettingsProvider } from './lite/ReviewSettings'
 import { ViewColumnResizer } from './lite/ViewColumnResizer'
@@ -6,7 +7,7 @@ import { ViewColumnResizer } from './lite/ViewColumnResizer'
 export default function App(): JSX.Element {
   return (
     <PhotoFindContextMenuProvider>
-      <ReviewSettingsProvider><LiteApp /><ViewColumnResizer /></ReviewSettingsProvider>
+      <ReviewSettingsProvider><LiteApp /><GlobalProcessStatus /><ViewColumnResizer /></ReviewSettingsProvider>
     </PhotoFindContextMenuProvider>
   )
 }
